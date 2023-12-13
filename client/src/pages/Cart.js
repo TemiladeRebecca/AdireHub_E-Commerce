81ff9a8f7529593d699bd18ae185b1dfeedb8055
Talabi Rebecca <122875790+TemiladeRebecca@users.noreply.github.com>
2023-12-13 19:34:35 +0100

    showing the list of items in cart

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import UserCartSidebar from "../components/cards/UserCartSidebar";
 import ProductCardHorizontal from "../components/cards/ProductCardHorizontal";
 
+
 export default function Cart() {
   // context
   const [cart, setCart] = useCart();
