e5003402978dcc377b6bc5d4de9687d272fca0f1
Talabi Rebecca <122875790+TemiladeRebecca@users.noreply.github.com>
2023-12-13 19:35:17 +0100

    list of categories

--- client/src/pages/CategoriesList.js
+++ client/src/pages/CategoriesList.js
@@ -2,6 +2,7 @@ import useCategory from "../hooks/useCategory";
 import Jumbotron from "../components/cards/jumbotron";
 import { Link } from "react-router-dom";
 
+
 export default function CategoriesList() {
   const categories = useCategory();
 
