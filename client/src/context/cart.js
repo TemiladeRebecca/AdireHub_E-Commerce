bfb4fd5d67e55273e9ce795dc1e48955775a22a0
TemiladeRebecca <temilade.rebecca20@gmail.com>
2024-01-31 00:21:57 +0100

    config files

--- client/src/context/cart.js
+++ client/src/context/cart.js
@@ -1,14 +1,27 @@
 import { useState, createContext, useContext, useEffect } from "react";
+import { useAuth } from "./auth";
 
 const CartContext = createContext();
 
 const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
+  const [loggedInUserId] = useAuth(); // Get the logged-in user's ID from the authentication context
 
   useEffect(() => {
-    let existingCart = localStorage.getItem("cart");
-    if (existingCart) setCart(JSON.parse(existingCart));
-  }, []);
+    const storedCart = localStorage.getItem('cart');
+    if (storedCart) {
+      const parsedCart = JSON.parse(storedCart);
+      // Check if the stored cart belongs to the logged-in user
+      if (parsedCart._id === loggedInUserId) {
+        setCart(parsedCart.items);
+        localStorage.setItem('cart')
+      } else {
+        setCart([]); // Reset cart for a new user on the same device
+        localStorage.removeItem('cart'); // Clear the cart for the previous user
+      }
+    }
+  }, [loggedInUserId]);
+
 
   return (
     <CartContext.Provider value={[cart, setCart]}>
